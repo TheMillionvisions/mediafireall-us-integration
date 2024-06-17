e9293e4cf4a1674a3ece7f0e6a777e516b56d20c
mndprdy <mani.deepu99@gmail.com>
2024-06-17 17:12:36 +0530

    update base url

---
+++ Python/API Request/GET Moderations Results By Time.py
@@ -0,0 +1,35 @@
+import requests
+
+# Base URL for the API
+BASE_URL = "BASE_URL_HERE"  # Provide the base URL here
+
+# User ID for authentication
+User_ID = "YOUR_USERID_HERE"  # Provide your user ID here
+
+# Start date and time in the format 'yyyy-mm-dd hh:mm:ss'
+Start_Date_Time = ''  # Provide the start date and time here
+
+# End date and time in the format 'yyyy-mm-dd hh:mm:ss'
+End_Date_Time = ''  # Provide the end date and time here
+
+
+url = f'https://{BASE_URL}/mfw/model/config/{User_ID}/all'
+
+
+# Parameters for the API request
+params = {
+    'complete': 'true',  # Provide 'true' or 'false' to filter completed items
+    'descend': 'true',   # Provide 'true' or 'false' to sort results in descending order
+    'end': End_Date_Time,  # Provide the end date and time
+    'pageNumber': 10,  # Provide the page number you want to view
+    'pageSize': 10,  # Provide the number of elements per page
+    'start': Start_Date_Time,  # Provide the start date and time
+    'apikey': 'YOUR_APIKEY_HERE',  # Provide your API key here
+}
+
+headers = {
+  'Authorization': 'Bearer YOUR_TOKEN_HERE',
+}
+
+response = requests.get(url, params=params, headers = headers)
+print(response.text)
--- Python/API Request/Post_Moderation_Request.py
+++ Python/API Request/Post_Moderation_Request.py
@@ -1,6 +1,10 @@
 import requests
 
-url = 'https://{BASE_URL}/moderation/{YOUR_ORG_ID_HERE}/{YOUR_USER_ID_HERE}/url/filters'
+BASE_URL = "BASE_URL_HERE" #provide the base url here
+User_ID = "YOUR_USERID_HERE" #provide your userId here
+Org_ID = "YOUR_ORGID_HERE" #provide your orgId here
+
+url = f'https://{BASE_URL}/moderation/{Org_ID}/{User_ID}/url/filters'
 
 
 params = {
